2cbce577b1a3cc8874dd7fee2a309e4795dbf63c
nayelieaguirre <99847013+nayelieaguirre@users.noreply.github.com>
2022-08-15 19:24:59 -0500

    More changes!

---
+++ js/index.js
@@ -0,0 +1,115 @@
+function onHoverSearch()
+{
+    $("#search").attr('src', 'images/search_icon2.tiff');
+}
+
+function offHoverSearch()
+{
+    $("#search").attr('src', 'images/search_icon.tiff');
+}
+
+
+
+function onHoverArrow()
+{
+    $(".arrow").attr('src', 'images/arrow2.tiff');
+}
+
+function offHoverArrow()
+{
+    $(".arrow").attr('src', 'images/arrow.tiff');
+}
+
+
+function onHoverDonation()
+{
+    $("#donation").attr('src', 'images/donation2.tiff');
+
+    document.getElementById('donation_text').style.color='#44C2D2';
+}
+
+function offHoverDonation()
+{
+    $("#donation").attr('src', 'images/donation.tiff');
+    
+    document.getElementById('donation_text').style.color='#414141';
+}
+
+
+function onHoverContact()
+{
+    $("#contact").attr('src', 'images/email2.tiff');
+
+    document.getElementById('contact_text').style.color='#44C2D2';
+}
+
+function offHoverContact()
+{
+    $("#contact").attr('src', 'images/email.tiff');
+
+    document.getElementById('contact_text').style.color='#414141';
+
+}
+
+function onHoverSupport()
+{
+    $("#support").attr('src', 'images/pin2.tiff');
+
+    document.getElementById('support_text').style.color='#44C2D2';
+}
+
+function offHoverSupport()
+{
+    $("#support").attr('src', 'images/pin.tiff');
+
+    document.getElementById('support_text').style.color='#414141';
+}
+
+function onHoverTwitter()
+{
+    $("#twitter").attr('src', 'images/twitter2.tiff');
+
+}
+
+function offHoverTwitter()
+{
+    $("#twitter").attr('src', 'images/twitter.tiff');
+
+}
+
+
+function onHoverFacebook()
+{
+    $("#facebook").attr('src', 'images/facebook2.tiff');
+
+}
+
+function offHoverFacebook()
+{
+    $("#facebook").attr('src', 'images/facebook.tiff');
+
+}
+
+function onHoverYoutube()
+{
+    $("#youtube").attr('src', 'images/youtube2.tiff');
+
+}
+
+function offHoverYoutube()
+{
+    $("#youtube").attr('src', 'images/youtube.tiff');
+
+}
+
+function onHoverLinkedin()
+{
+    $("#linkedin").attr('src', 'images/linkedin2.tiff');
+
+}
+
+function offHoverLinkedin()
+{
+    $("#linkedin").attr('src', 'images/linkedin.tiff');
+
+}
