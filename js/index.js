c403ffb3c314b086f75b70920bbd5a1877b0fb49
nayelieaguirre <99847013+nayelieaguirre@users.noreply.github.com>
2022-08-17 20:25:38 -0500

    change .tiff to .jpg

--- js/index.js
+++ js/index.js
@@ -1,36 +1,36 @@
 function onHoverSearch()
 {
-    $("#search").attr('src', 'images/search_icon2.tiff');
+    $("#search").attr('src', 'images/search_icon2.png');
 }
 
 function offHoverSearch()
 {
-    $("#search").attr('src', 'images/search_icon.tiff');
+    $("#search").attr('src', 'images/search_icon.png');
 }
 
 
 
 function onHoverArrow()
 {
-    $(".arrow").attr('src', 'images/arrow2.tiff');
+    $(".arrow").attr('src', 'images/arrow2.png');
 }
 
 function offHoverArrow()
 {
-    $(".arrow").attr('src', 'images/arrow.tiff');
+    $(".arrow").attr('src', 'images/arrow.png');
 }
 
 
 function onHoverDonation()
 {
-    $("#donation").attr('src', 'images/donation2.tiff');
+    $("#donation").attr('src', 'images/donation2.png');
 
     document.getElementById('donation_text').style.color='#44C2D2';
 }
 
 function offHoverDonation()
 {
-    $("#donation").attr('src', 'images/donation.tiff');
+    $("#donation").attr('src', 'images/donation.png');
     
     document.getElementById('donation_text').style.color='#414141';
 }
@@ -38,14 +38,14 @@ function offHoverDonation()
 
 function onHoverContact()
 {
-    $("#contact").attr('src', 'images/email2.tiff');
+    $("#contact").attr('src', 'images/email2.png');
 
     document.getElementById('contact_text').style.color='#44C2D2';
 }
 
 function offHoverContact()
 {
-    $("#contact").attr('src', 'images/email.tiff');
+    $("#contact").attr('src', 'images/email.png');
 
     document.getElementById('contact_text').style.color='#414141';
 
@@ -53,63 +53,63 @@ function offHoverContact()
 
 function onHoverSupport()
 {
-    $("#support").attr('src', 'images/pin2.tiff');
+    $("#support").attr('src', 'images/pin2.png');
 
     document.getElementById('support_text').style.color='#44C2D2';
 }
 
 function offHoverSupport()
 {
-    $("#support").attr('src', 'images/pin.tiff');
+    $("#support").attr('src', 'images/pin.png');
 
     document.getElementById('support_text').style.color='#414141';
 }
 
 function onHoverTwitter()
 {
-    $("#twitter").attr('src', 'images/twitter2.tiff');
+    $("#twitter").attr('src', 'images/twitter2.png');
 
 }
 
 function offHoverTwitter()
 {
-    $("#twitter").attr('src', 'images/twitter.tiff');
+    $("#twitter").attr('src', 'images/twitter.png');
 
 }
 
 
 function onHoverFacebook()
 {
-    $("#facebook").attr('src', 'images/facebook2.tiff');
+    $("#facebook").attr('src', 'images/facebook2.png');
 
 }
 
 function offHoverFacebook()
 {
-    $("#facebook").attr('src', 'images/facebook.tiff');
+    $("#facebook").attr('src', 'images/facebook.png');
 
 }
 
 function onHoverYoutube()
 {
-    $("#youtube").attr('src', 'images/youtube2.tiff');
+    $("#youtube").attr('src', 'images/youtube2.png');
 
 }
 
 function offHoverYoutube()
 {
-    $("#youtube").attr('src', 'images/youtube.tiff');
+    $("#youtube").attr('src', 'images/youtube.png');
 
 }
 
 function onHoverLinkedin()
 {
-    $("#linkedin").attr('src', 'images/linkedin2.tiff');
+    $("#linkedin").attr('src', 'images/linkedin2.png');
 
 }
 
 function offHoverLinkedin()
 {
-    $("#linkedin").attr('src', 'images/linkedin.tiff');
+    $("#linkedin").attr('src', 'images/linkedin.png');
 
 }
